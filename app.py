81f682dcbf8398806dc715350e0739aff9d29362
daspartho <parthodas6176@gmail.com>
2022-12-25 02:30:46 +0530

    kmax default 0.6

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ iface = gr.Interface(
         gr.Image(shape=(512,512), type="pil"),
         gr.Text(),
         gr.Slider(value=0.3,minimum=0, maximum=1, step=0.1),
-        gr.Slider(value=0.5,minimum=0, maximum=1, step=0.1),
+        gr.Slider(value=0.6,minimum=0, maximum=1, step=0.1),
         gr.Slider(value=0.5,minimum=0, maximum=1, step=0.1),
         gr.Number(value=42, maximum=2**64-1),
         gr.Slider(value=50),
